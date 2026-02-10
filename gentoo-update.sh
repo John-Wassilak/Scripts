933a413728ff52c9b75668421bf3ef2b39273897
John Wassilak <john@wassilak.com>
2026-02-10 09:02:07 -0600

    auto updating git repos

--- gentoo-update.sh
+++ gentoo-update.sh
@@ -12,3 +12,12 @@ sudo bash -c "emerge --sync && \
               emerge --ask --update --deep --newuse $EXCLUDE @world && \
               emerge --depclean && \
               revdep-rebuild"
+
+echo ""
+echo "updating git repos"
+
+cd -v /home/john/packages/dms
+PRE_PULL=$(git rev-parse HEAD); git pull && [ "$PRE_PULL" != "$(git rev-parse HEAD)" ] && sudo make install
+
+cd -v /home/john/packages/yt-dlp
+git pull
